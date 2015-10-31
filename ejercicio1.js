4a7677e7bd8cbd3a842d6818197fc4cb285a059d
MarisolenlaWeb <marcarrimor@gmail.com>
2015-10-30 23:48:16 -0600

    Ejercicios Diapositiva 74

---
+++ ejercicio1.js
@@ -0,0 +1,18 @@
+//Escribe una función que suma todos los dígitos en un array y 
+//otra que multiplique todos los números de otra array.
+
+var numeros = [10,26,15,94];
+
+function suma(){
+  console.log(numeros[0]+numeros[1]+numeros[2]+numeros[3]);
+} 
+suma();
+
+//Multiplicación
+
+var numeros = [10,26,15,94];
+
+function suma(){
+  console.log(numeros[0]*numeros[1]*numeros[2]*numeros[3]);
+} 
+suma();
---
+++ ejercicio2.js
@@ -0,0 +1,7 @@
+//Escribe una función que tome un string y lo devuelva reverso.
+
+function stringreverse(){
+var string = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+   console.log(string.reverse());
+}  
+stringreverse();
